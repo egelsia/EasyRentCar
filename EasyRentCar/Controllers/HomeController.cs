50b750e03b1d7df4c1a2a8c739ed64981b2043dd
Hakan Karaduman <70797184+egelsia@users.noreply.github.com>
2023-12-16 21:28:06 +0300

    Started to build the website GUI. Added HomeController.
    Added css files to improve visuals of the website. Created HomeController and added Contact page for contact information.

---
+++ EasyRentCar/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EasyRentCar.Models.EntityFramework;
+
+namespace EasyRentCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        carDBEntities db = new carDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+    }
+}

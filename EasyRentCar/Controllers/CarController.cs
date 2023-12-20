67a8835a12c04a3431b248a01b269178975e7830
Hakan Karaduman <70797184+egelsia@users.noreply.github.com>
2023-12-20 15:13:28 +0300

    Created CarController and ErrorController
    Created CarController for the process of cars. Created ErrorController for 404 errors. Updated Web.config file.

---
+++ EasyRentCar/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EasyRentCar.Models.EntityFramework;
+
+namespace EasyRentCar.Controllers
+{
+    public class CarController : Controller
+    {
+        carDBEntities db = new carDBEntities();
+        // GET: Car
+        public ActionResult Index()
+        {
+            var carList = db.CARs.ToList();
+            var model = carList.OrderBy(m => m.CAR_ID).ToList();
+            return View(model);
+        }
+    }
+}
